51e01b5d6e984c09bd3331a7a069a172a8339a03
LASHA KATCHEISHVILI <katcheishvili.l@gmail.com>
2022-02-03 23:29:36 +0400

    change-controller

--- BalanceManagement/Controllers/BalanceController.cs
+++ BalanceManagement/Controllers/BalanceController.cs
@@ -17,19 +17,10 @@ namespace BalanceManagement.Controllers
             _gameBalanceManager = new GameManagementService();
         }
         [HttpGet("balance")]
-        public decimal GetBalance()
-        {
-            return _gameBalanceManager.GetBalanceCasino();
-        }
+        public decimal GetBalance()=> _gameBalanceManager.GetBalanceCasino();
         [HttpPost("withdrawal/{transactionId}/{amount}")]
-        public BaseResponseModel TransferMoneyToPlayer(string transactionId, decimal amount)
-        {
-            return _gameBalanceManager.TransferMoneyToPlayer(new ServiceModels.TransferModel.TransferMoneyRequestModel(transactionId,amount));
-        }
+        public BaseResponseModel TransferMoneyToPlayer(string transactionId, decimal amount) => _gameBalanceManager.TransferMoneyToPlayer(new ServiceModels.TransferModel.TransferMoneyRequestModel(transactionId, amount));
         [HttpPost("deposit/{transactionId}/{amount}")]
-        public BaseResponseModel TransferMoneyToCasino(string transactionId, decimal amount)
-        {
-          return _gameBalanceManager.TransferMoneyToCasino(new ServiceModels.TransferModel.TransferMoneyRequestModel(transactionId, amount));
-        }
+        public BaseResponseModel TransferMoneyToCasino(string transactionId, decimal amount)=> _gameBalanceManager.TransferMoneyToCasino(new ServiceModels.TransferModel.TransferMoneyRequestModel(transactionId, amount));
     }
 }
